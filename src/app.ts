0f187e83f409288c399544e5bbdd8fb98204e7cc
manik-babu <mdmanikbabu48@gmail.com>
2025-12-28 22:22:33 +0600

    Setup better-auth

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import express, { Application, Request, Response } from 'express';
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
 
 const app: Application = express();
+
+// better auth
+app.use(express.json());
+app.use(express.urlencoded());
+app.all('/api/auth/*splat', toNodeHandler(auth));
+
 
 app.get('/', async (req: Request, res: Response) => {
     res.json({
