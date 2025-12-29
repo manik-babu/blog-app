7aca6d06db41bbcd15acd9d96f561dce7753735e
manik-babu <mdmanikbabu48@gmail.com>
2025-12-29 21:53:01 +0600

    Update auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -8,30 +8,40 @@ export enum UserRole {
 }
 const auth = (...roles: UserRole[]) => {
     return async (req: Request, res: Response, next: NextFunction) => {
-        const session = await betterAuth.api.getSession({
-            headers: req.headers as any
-        })
-        console.log(session)
-        if (!session) {
-            return res.status(401).json({
-                message: "You are not authorized"
-            });
-        }
-        if (!roles.includes(session.user.role as UserRole)) {
-            return res.status(401).json({
-                message: "You are not authorized"
+        try {
+            const session = await betterAuth.api.getSession({
+                headers: req.headers as any
+            })
+
+            if (!session) {
+                return res.status(401).json({
+                    message: "You are not authorized"
+                });
+            }
+            if (!roles.includes(session.user.role as UserRole)) {
+                return res.status(401).json({
+                    message: "You are not authorized"
+                });
+            }
+
+            req.user = {
+                id: session.user.id,
+                name: session.user.name,
+                email: session.user.email,
+                role: session.user.role as string,
+                emailVerified: session.user.emailVerified
+            }
+
+            next();
+        } catch (error: any) {
+            console.error('Server error: ', error.message);
+            res.status(500).json({
+                success: false,
+                message: 'Internal server error!',
+                errors: error.message
             });
         }
 
-        req.user = {
-            id: session.user.id,
-            name: session.user.name,
-            email: session.user.email,
-            role: session.user.role as string,
-            emailVerified: session.user.emailVerified
-        }
-
-        next();
     }
 }
 
